786738733a3b852ae09805575423393b28efb6f9
RavinduTharanga <113649586+RavinduTharanga@users.noreply.github.com>
2023-06-11 10:25:12 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -19,15 +19,15 @@ void display_board(){
     //Rander Game Board LAYOUT
 
     cout<<"PLAYER - 1 [X]t PLAYER - 2 [O]nn";
-    cout<<"tt     |     |     n";
-    cout<<"tt  "<<board[0][0]<<"  | "<<board[0][1]<<"  |  "<<board[0][2]<<" n";
-    cout<<"tt_____|_____|_____n";
-    cout<<"tt     |     |     n";
-    cout<<"tt  "<<board[1][0]<<"  | "<<board[1][1]<<"  |  "<<board[1][2]<<" n";
-    cout<<"tt_____|_____|_____n";
-    cout<<"tt     |     |     n";
-    cout<<"tt  "<<board[2][0]<<"  | "<<board[2][1]<<"  |  "<<board[2][2]<<" n";
-    cout<<"tt     |     |     n";
+    cout<<"     |     |     \n";
+    cout<<" "<<board[0][0]<<"  | "<<board[0][1]<<"  |  "<<board[0][2]<<" \n";
+    cout<<"_____|_____|_____\n";
+    cout<<"     |     |     \n";
+    cout<<" "<<board[1][0]<<"  | "<<board[1][1]<<"  |  "<<board[1][2]<<" \n";
+    cout<<"_____|_____|_____\n";
+    cout<<"     |     |     \n";
+    cout<<"  "<<board[2][0]<<"  | "<<board[2][1]<<"  |  "<<board[2][2]<<" \n";
+    cout<<"     |     |     \n";
 }
 
 //Function to get the player input and update the board
