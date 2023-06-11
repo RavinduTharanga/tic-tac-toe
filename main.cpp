e2c1f5a3136e6242a6a3360a7fa94346bbbf674b
RavinduTharanga <113649586+RavinduTharanga@users.noreply.github.com>
2023-06-11 14:47:56 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -48,7 +48,7 @@ void player_turn(){
 
     if(turn == 'x' && board[row][column] != 'x' && board[row][column] != 'o'){
         board[row][column] ='x';
-        turn == 'o';
+        turn = 'o';
     }
     else if(turn == 'o' && board[row][column] != 'x' && board[row][column] != 'O'){
         //updating the position for 'O' symbol if
@@ -62,21 +62,29 @@ void player_turn(){
         player_turn();
     }
     
-    display_board();
 }
 
-bool game_over(){
-    for(i=0,i<3,i++){
-        if(board[i][0] == board[i][1] && board[i][0] == board[i][2] || board[0][i] == board[1][i] && board[0][i] == board[2][i])
-        return false;
-        if(board[0][0] == board[1][1] && board[0][0] == board[2][2] || board[0][2] == board[1][1] && board[0][2] == board[2][0])
-        return false;
-    }
-    
+bool gameover(){
+    //checking the win for Simple Rows and Simple Column
+    for(int i=0; i<3; i++)
+    if(board[i][0] == board[i][1] && board[i][0] == board[i][2] || board[0][i] == board[1][i] && board[0][i] == board[2][i])
+    return false;
 
+    //checking the win for both diagonal
+
+    if(board[0][0] == board[1][1] && board[0][0] == board[2][2] || board[0][2] == board[1][1] && board[0][2] == board[2][0])
+    return false;
+
+    //Checking the game is in continue mode or not
+    for(int i=0; i<3; i++)
+    for(int j=0; j<3; j++)
+    if(board[i][j] != 'x' && board[i][j] != '0')
+    return true;
+
+    //Checking the if game already draw
+    draw = true;
+    // return false;
 }
-
-//Program Main Method
 
 int main()
 {
@@ -87,11 +95,11 @@ int main()
         player_turn();
         gameover();
     }
-    if(turn == 'X' && draw == false){
-        cout<<"nnCongratulations!Player with 'X' has won the game";
+    if(turn == 'x' && draw == false){
+        cout<<"nnCongratulations!Player with 'X' has won the game\n";
     }
-    else if(turn == 'O' && draw == false){
-        cout<<"nnCongratulations!Player with 'O' has won the game";
+    else if(turn == 'o' && draw == false){
+        cout<<"nnCongratulations!Player with 'O' has won the game\n";
     }
     else
     cout<<"nnGAME DRAW!!!nn";
